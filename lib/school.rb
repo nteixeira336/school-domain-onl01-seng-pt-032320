618328b02a07bebd9dee7b1b9b8abc33d1cef4a4
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-04-17 14:43:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School 
+  
+  attr_accessor :name :roster 
+  
+  def initialize(name, roster)
+    @name=name 
+    @roster={}
+  end 
+  
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade] << student 
+  end 
+  
+  def grade(grade)
+    
+  end 
+  
+  def sort 
+    self.sort
+  end 
+    
+end 
